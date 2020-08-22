125769b1b080714972f1a9347e3a2fb4ffe386ba
Rafa Lyovson <rafa@lyovson.com>
2020-08-23 03:07:37 +0400

    changed a string for testing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Damn, Daniel!");
 });
 
 app.listen(port, () => console.log(`Running on port ${port}!`));
